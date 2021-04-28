3550c08101b2b2f875d65270dac4aab52b763681
lukamacho <lmach19@freeuni.edu.ge>
2021-04-28 21:06:14 +0400

    Second Milestone

--- Board.java
+++ Board.java
@@ -1,4 +1,5 @@
-// Board.java
+
+import java.util.Arrays;
 
 /**
  CS108 Tetris Board.
@@ -13,10 +14,16 @@ public class Board	{
 	private int width;
 	private int height;
 	private boolean[][] grid;
-	private boolean DEBUG = true;
+	protected boolean DEBUG = true;
 	boolean committed;
-	
-	
+    private int maxColumnHeight;
+	private int [] widths;
+	private int [] heights;
+
+	private boolean [][] backupGrid;
+	private  int [] backupWidths;
+	private int  [] backupHeights;
+	private int backupMaxColumnHeight;
 	// Here a few trivial methods are provided:
 	
 	/**
@@ -28,8 +35,14 @@ public class Board	{
 		this.height = height;
 		grid = new boolean[width][height];
 		committed = true;
-		
-		// YOUR CODE HERE
+		widths = new int[height];
+		heights = new int[width];
+		maxColumnHeight = 0;
+
+		backupGrid = new boolean[width][height];
+		backupHeights = new int[width];
+		backupWidths = new int [height];
+		backupMaxColumnHeight = 0;
 	}
 	
 	
@@ -53,8 +66,8 @@ public class Board	{
 	 Returns the max column height present in the board.
 	 For an empty board this is 0.
 	*/
-	public int getMaxHeight() {	 
-		return 0; // YOUR CODE HERE
+	public int getMaxHeight() {
+		return maxColumnHeight;
 	}
 	
 	
@@ -64,7 +77,33 @@ public class Board	{
 	*/
 	public void sanityCheck() {
 		if (DEBUG) {
-			// YOUR CODE HERE
+			int curMaxColumnHeight = 0;
+			int [] curWidths = new int [height];
+			int [] curHeights = new int [width];
+
+			for(int i = 0; i < width; i++){
+				for(int j =0; j < height; j++){
+					if(grid[i][j]){
+						curWidths[j]++;
+						if(curHeights[i]<=j){
+							curHeights[i] = j + 1;
+							if(curHeights[i] > curMaxColumnHeight){
+								curMaxColumnHeight = curHeights[i];
+							}
+						}
+					}
+				}
+			}
+			if(!Arrays.equals(curWidths,widths)){
+				throw new RuntimeException("Incorrect width array");
+			}
+			if(!Arrays.equals(curHeights,heights)){
+				throw new RuntimeException("Incorrect height array");
+			}
+			if(curMaxColumnHeight != maxColumnHeight){
+				System.out.println(curMaxColumnHeight+ " "+maxColumnHeight);
+				throw new RuntimeException("Incorrect maxColumnHeight");
+			}
 		}
 	}
 	
@@ -78,7 +117,14 @@ public class Board	{
 	 to compute this fast -- O(skirt length).
 	*/
 	public int dropHeight(Piece piece, int x) {
-		return 0; // YOUR CODE HERE
+		int dropPoint = 0;
+		for(int i = 0; i < piece.getWidth(); i++){
+			int currentDropPoint = heights[x + i] - piece.getSkirt()[i];
+			if(dropPoint < currentDropPoint){
+				dropPoint = currentDropPoint;
+			}
+		}
+		return dropPoint;
 	}
 	
 	
@@ -88,7 +134,7 @@ public class Board	{
 	 The height is 0 if the column contains no blocks.
 	*/
 	public int getColumnHeight(int x) {
-		return 0; // YOUR CODE HERE
+		return heights[x];
 	}
 	
 	
@@ -97,7 +143,7 @@ public class Board	{
 	 the given row.
 	*/
 	public int getRowWidth(int y) {
-		 return 0; // YOUR CODE HERE
+		return widths[y];
 	}
 	
 	
@@ -107,9 +153,16 @@ public class Board	{
 	 always return true.
 	*/
 	public boolean getGrid(int x, int y) {
-		return false; // YOUR CODE HERE
+		return (!isValid(x,y) || grid[x][y]);
 	}
-	
+
+	/**
+	 * This function gets two integers of the point and return true
+	 * if it is in the grid or false otherwise.
+	 */
+	 private boolean isValid(int x,int y){
+		return x > -1 && y > -1 && x < width && y<height;
+	 }
 	
 	public static final int PLACE_OK = 0;
 	public static final int PLACE_ROW_FILLED = 1;
@@ -133,26 +186,95 @@ public class Board	{
 	public int place(Piece piece, int x, int y) {
 		// flag !committed problem
 		if (!committed) throw new RuntimeException("place commit problem");
-			
+		previousGrid();
 		int result = PLACE_OK;
-		
-		// YOUR CODE HERE
-		
+		committed = false;
+		for(int i=0; i< piece.getBody().length;i++){
+			TPoint cur = piece.getBody()[i];
+
+			int curX = cur.x + x;
+			int curY = cur.y + y;
+			if(!isValid(curX,curY)){
+				return PLACE_OUT_BOUNDS;
+			}
+			if(grid[curX][curY]){
+				return PLACE_BAD;
+			}
+			grid[curX][curY] = true;
+			widths[curY]++;
+			heights[curX] = Math.max(curY + 1,heights[curX]);
+			if(maxColumnHeight < heights[curX]){
+				maxColumnHeight = heights[curX];
+			}
+			if(widths[curY] == width){
+				result = PLACE_ROW_FILLED;
+			}
+		}
+		sanityCheck();
 		return result;
 	}
-	
-	
+
+	private void previousGrid() {
+		for(int i=0; i<width; i++){
+			for(int j=0;j<height;j++){
+				backupGrid[i][j] = grid[i][j];
+			}
+		}
+		backupMaxColumnHeight = maxColumnHeight;
+		for(int i=0;i<height;i++){
+			backupWidths[i] = widths[i];
+		}
+		for(int i=0;i<width;i++){
+			backupHeights[i] = heights[i];
+		}
+	}
+
+
 	/**
 	 Deletes rows that are filled all the way across, moving
 	 things above down. Returns the number of rows cleared.
 	*/
 	public int clearRows() {
 		int rowsCleared = 0;
-		// YOUR CODE HERE
+		committed = false;
+		int toRow =0;
+		for(int fromRow = 0;fromRow < height;fromRow++){
+			if(widths[fromRow] == width){
+				rowsCleared++;
+			}else if(rowsCleared > 0){
+				widths[toRow] = widths[fromRow];
+				for(int i = 0; i < width; i++){
+					grid[i][toRow] = grid[i][fromRow];
+				}
+				toRow++;
+			}else{
+				toRow++;
+			}
+		}
+		while(toRow<height){
+			widths[toRow]=0;
+			for(int i=0;i<width;i++){
+				grid[i][toRow]=false;
+				heights[i]--;
+			}
+			toRow++;
+			maxColumnHeight--;
+		}
+		for(int i=0;i<width;i++){
+			boolean exist = false;
+			for(int j=0;j<height;j++){
+				if(grid[i][j]){
+					exist=true;
+					break;
+				}
+			}
+			if(!exist) {
+				heights[i] = 0;
+			}
+		}
 		sanityCheck();
 		return rowsCleared;
 	}
-
 
 
 	/**
@@ -163,7 +285,26 @@ public class Board	{
 	 See the overview docs.
 	*/
 	public void undo() {
-		// YOUR CODE HERE
+		if(committed){
+			return;
+		}
+		boolean [][] tempGrid = grid;
+		grid = backupGrid;
+		backupGrid = tempGrid;
+
+		int [] tempWidths = widths;
+		widths = backupWidths;
+		backupWidths = tempWidths;
+
+		int [] tempHeights = heights;
+		heights = backupHeights;
+		backupHeights = tempHeights;
+
+		int tempMaxColumnHeight = maxColumnHeight;
+		maxColumnHeight = backupMaxColumnHeight;
+		backupMaxColumnHeight = tempMaxColumnHeight;
+
+		commit();
 	}
 	
 	
@@ -175,8 +316,10 @@ public class Board	{
 	}
 
 
-	
-	/*
+	public void changeDebugMode(){
+		DEBUG = !DEBUG;
+	}
+	/**
 	 Renders the board state as a big String, suitable for printing.
 	 This is the sort of print-obj-state utility that can help see complex
 	 state change over time.
